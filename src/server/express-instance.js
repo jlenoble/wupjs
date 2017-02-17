283cad733351adc3a7f646210b01d312b516037e
Jason Lenoble <jason.lenoble@gmail.com>
2017-02-17 22:39:00 +0100

    Moved views/index.ejs

--- src/server/express-instance.js
+++ src/server/express-instance.js
@@ -4,7 +4,7 @@ import Express from 'express';
 const app = new Express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../../../src/views'));
+app.set('views', path.join(__dirname, '../../../src/server/views'));
 
 app.use(Express.static(path.join(__dirname, '..')));
 app.use(Express.static(path.join(__dirname, '../..')));
