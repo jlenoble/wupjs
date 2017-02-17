cc602e076e81824811211e53076910f423ee3a10
Jason Lenoble <jason.lenoble@gmail.com>
2017-02-18 00:57:02 +0100

    Removed all TestPage deps
    also added body-parser to Express

--- src/components/test-page.jsx
+++ src/components/test-page.jsx
@@ -1,14 +1,9 @@
 import React, {Component} from 'react';
-import AddItem from './test/add-item';
-import VisibleList from './test/visible-list';
-import Footer from './test/footer';
 
 export default class TestPage extends Component {
   render () {
     return <div>
-      <AddItem/>
-      <VisibleList/>
-      <Footer/>
+      Hello world
     </div>;
   }
 }
--- src/server/express-instance.js
+++ src/server/express-instance.js
@@ -1,10 +1,13 @@
 import path from 'path';
 import Express from 'express';
+import bodyParser from 'body-parser';
 
 const app = new Express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../../../src/server/views'));
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(Express.static(path.join(__dirname, '..')));
 app.use(Express.static(path.join(__dirname, '../..')));
