de2e100e4c2d4b9d199e0340c2dc1f4e13f5cf06
Jason Lenoble <jason.lenoble@gmail.com>
2017-02-18 15:13:49 +0100

    Renamed index.js to fetch-items.js
    New index.js reexports fetch-items.js exports

---
+++ src/actions/fetch-items.js
@@ -0,0 +1,51 @@
+import fetch from 'isomorphic-fetch';
+
+export const REQUEST_ITEMS = 'REQUEST_ITEMS';
+function requestItems () {
+  return {
+    type: REQUEST_ITEMS,
+  };
+}
+
+export const RECEIVE_ITEMS_SUCCESS = 'RECEIVE_ITEMS_SUCCESS';
+function receiveItemsSuccess (json) {
+  return {
+    type: RECEIVE_ITEMS_SUCCESS,
+    items: json,
+  };
+}
+
+export const RECEIVE_ITEMS_ERROR = 'RECEIVE_ITEMS_ERROR';
+function receiveItemsError (error) {
+  return {
+    type: RECEIVE_ITEMS_ERROR,
+    error,
+  };
+}
+
+function fetchItems () {
+  return dispatch => {
+    dispatch(requestItems());
+
+    return fetch('/api/items')
+      .then(
+        response => response.json(),
+        err => dispatch(receiveItemsError(err))
+      ).then(
+        json => dispatch(receiveItemsSuccess(json))
+      );
+  };
+}
+
+function shouldFetchItems (state) {
+  const {items} = state;
+  return !items || (items.items.length === 0 && !items.isFetching);
+}
+
+export function fetchItemsIfNeeded () {
+  return (dispatch, getState) => {
+    if (shouldFetchItems(getState())) {
+      return dispatch(fetchItems());
+    }
+  };
+}
