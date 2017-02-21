cea58ab64b966ed1528d98336e2c0a64923c6ce9
Jason Lenoble <jason.lenoble@gmail.com>
2017-02-21 12:59:55 +0100

    addClass to add class to button
    needed to pass active class down to button node

--- src/components/list/item-buttons.jsx
+++ src/components/list/item-buttons.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import ActionGlyphButton from '../container/action-glyph-button';
 import {editItem, deleteItem, unfocusCurrentItem} from '../../actions';
 import {itemPropType} from './proptypes';
 
 const makeItemButton = (applyToItem, glyphicon) => {
-  const ItemButton = ({item}) => (
+  const ItemButton = ({item, addClass}) => (
     <ActionGlyphButton
       handleClick={dispatch => {
         dispatch(unfocusCurrentItem());
         dispatch(applyToItem(item));
       }}
       glyphicon={glyphicon}
+      addClass={addClass}
     />
   );
 
   ItemButton.propTypes = {
     item: itemPropType.isRequired,
+    addClass: PropTypes.string,
   };
 
   return ItemButton;
