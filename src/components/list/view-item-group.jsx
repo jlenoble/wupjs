e5873d043c799a706d1c09d8533007df01873f50
Jason Lenoble <jason.lenoble@gmail.com>
2017-02-22 18:26:02 +0100

    defaults for isSelected/isScheduled

--- src/components/list/view-item-group.jsx
+++ src/components/list/view-item-group.jsx
@@ -4,27 +4,34 @@ import GlyphButtonGroup from '../presentational/glyph-button-group';
 import {EditItemButton, RemoveItemButton} from './item-buttons';
 import {SelectItemCheckbox, ScheduleItemCkeckbox} from './item-checkboxes';
 
-const ViewItemGroup = ({item}) => (
-  <div className="col">
-    <span className="row vertical-align">
-      <SelectItemCheckbox
-        addClass="col toggle-selected"
-        item={item}
-      />
-      <ScheduleItemCkeckbox
-        addClass="col toggle-scheduled"
-        item={item}
-      />
-      <span className="col">
-        {item.title}
+const ViewItemGroup = ({item}) => {
+  const _item = Object.assign({
+    isSelected: false,
+    isScheduled: false,
+  }, item);
+
+  return (
+    <div className="col">
+      <span className="row vertical-align">
+        <SelectItemCheckbox
+          addClass="col"
+          item={_item}
+        />
+        <ScheduleItemCkeckbox
+          addClass="col"
+          item={_item}
+        />
+        <span className="col">
+          {_item.title}
+        </span>
+        <GlyphButtonGroup addClass="col justify-content-end">
+          <EditItemButton item={_item}/>
+          <RemoveItemButton item={_item}/>
+        </GlyphButtonGroup>
       </span>
-      <GlyphButtonGroup addClass="col justify-content-end">
-        <EditItemButton item={item}/>
-        <RemoveItemButton item={item}/>
-      </GlyphButtonGroup>
-    </span>
-  </div>
-);
+    </div>
+  );
+};
 
 ViewItemGroup.propTypes = {
   item: itemPropType.isRequired,
