633e64d4e94e7e872f9f399ad23740da8d14cd4d
Jason Lenoble <jason.lenoble@gmail.com>
2017-02-22 16:34:34 +0100

    takes stem, returns isStemed

---
+++ src/helpers/make-is-prop.js
@@ -0,0 +1,14 @@
+export default stem => {
+  let _stem = stem.toLowerCase();
+  let len = stem.length;
+
+  if (_stem.slice(len - 2) === 'ed') {
+    _stem = _stem.slice(0, len - 2);
+  }
+
+  if (_stem.slice(0, 2) === 'is') {
+    _stem = _stem.slice(2);
+  }
+
+  return 'is' + _stem[0].toUpperCase() + _stem.slice(1) + 'ed';
+};
