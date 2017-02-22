540525652b8480f0e89048be4711fe4e1f903132
Jason Lenoble <jason.lenoble@gmail.com>
2017-02-22 16:32:17 +0100

    item(s)WithRequiredProps helpers

--- src/components/list/proptypes.js
+++ src/components/list/proptypes.js
@@ -1,8 +1,21 @@
 import {PropTypes} from 'react';
 
-export const itemPropType = PropTypes.shape({
+const requiredPropTypes = {
   title: PropTypes.string.isRequired,
   _id: PropTypes.string.isRequired,
-});
+};
 
+export const itemPropType = PropTypes.shape(requiredPropTypes);
 export const itemsPropType = PropTypes.arrayOf(itemPropType.isRequired);
+
+export const itemWithRequiredProps = props => {
+  const _props = Object.assign({}, requiredPropTypes);
+  Object.keys(props).forEach(key => {
+    _props[key] = PropTypes[props[key]].isRequired;
+  });
+  return PropTypes.shape(_props);
+};
+
+export const itemsWithRequiredProps = props => {
+  return PropTypes.arrayOf(itemWithRequiredProps(props).isRequired);
+};
