183e34ba1d84b8699d48a7496b10ff5a37d403f6
Jason Lenoble <jason.lenoble@gmail.com>
2017-02-24 02:32:37 +0100

    Fixed case children is not an array

--- src/components/layouts/one-row.jsx
+++ src/components/layouts/one-row.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
-export default ({children}) => (
-  <div className="row">
-    {children.map((child, i) => (
-      <div
-        className="col"
-        key={'wup-' + i}
-      >
-        {child}
-      </div>
-    ))}
-  </div>
-);
+export default ({children}) => {
+  const makeComponent = (child, i) => (
+    <div
+      className="col"
+      key={'wup-' + i}
+    >
+      {child}
+    </div>
+  );
+
+  return (
+    <div className="row">
+      {Array.isArray(children) ? children.map(makeComponent) :
+        makeComponent(children, 0)}
+    </div>
+  );
+};
