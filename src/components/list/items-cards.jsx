586c11a91fc1b75a5ac21ca29f6233923e059b73
Jason Lenoble <jason.lenoble@gmail.com>
2017-02-25 00:11:14 +0100

    Factored out Cards into a factory

--- src/components/list/items-cards.jsx
+++ src/components/list/items-cards.jsx
@@ -2,61 +2,123 @@ import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {fetchItemsIfNeeded} from '../../actions';
 import CardBlock from './card-block';
+import {AddItemInputGroup} from './item-input-groups';
+import ButtonGroup from '../presentational/button-group';
 import ActionGlyphButton from '../container/action-glyph-button';
-import {EditItemButton, UnselectItemButton} from './item-buttons';
+import {EditItemButton, DeleteItemButton, UnselectItemButton}
+  from './item-buttons';
+import {SelectItemCheckbox, ScheduleItemCkeckbox} from './item-checkboxes';
+import {EditSwitchButton, DeleteSwitchButton, SelectSwitchButton,
+  ScheduleSwitchButton} from './switch-buttons';
+import {setFuncName} from '../../helpers';
 
-class Card extends Component {
-  componentDidMount () {
-    this.props.dispatch(fetchItemsIfNeeded());
-  }
+const makeCard = ({mapStateToProps, itemUi, headerContent}) => {
+  class Card extends Component {
+    componentDidMount () {
+      this.props.dispatch(fetchItemsIfNeeded());
+    }
 
-  render () {
-    const {items, isFetching} = this.props;
+    render () {
+      const {items, isFetching} = this.props;
 
-    return (
-      <div className="card">
-        <div className="card-header">
-          <ActionGlyphButton
-            glyphicon="save"
-            handleClick={dispatch => {
-            }}
+      return (
+        <div className="card">
+          <div className="card-header">
+            {headerContent}
+          </div>
+          <CardBlock
+            items={items}
+            isFetching={isFetching}
+            ui={itemUi}
           />
         </div>
-        <CardBlock
-          items={items}
-          isFetching={isFetching}
-          ui={{
-            buttons: [EditItemButton, UnselectItemButton],
-            checkboxes: [],
-          }}
-        />
-      </div>
-    );
+      );
+    }
   }
-}
 
-Card.propTypes = {
-  items: PropTypes.array.isRequired,
-  isFetching: PropTypes.bool.isRequired,
+  Card.propTypes = {
+    items: PropTypes.array.isRequired,
+    isFetching: PropTypes.bool.isRequired,
+  };
+
+  return connect(mapStateToProps)(Card);
 };
 
-function mapStateToProps (state) {
-  const props = Object.assign({}, state.items);
-  const items = props.items;
-  const selection = state.currentSelection.items;
+const CurrentSelectionCard = makeCard({
+  headerContent: (
+    <ActionGlyphButton
+      glyphicon="save"
+      handleClick={dispatch => {
+      }}
+    />
+  ),
+  itemUi: {
+    buttons: [EditItemButton, UnselectItemButton],
+    checkboxes: [],
+  },
+  mapStateToProps: state => {
+    const props = Object.assign({}, state.items);
+    const items = props.items;
+    const selection = state.currentSelection.items;
 
-  props.items = Object.keys(items).filter(_id => {
-    return selection[_id];
-  }).map(_id => Object.assign({
-    isSelected: true,
-  }, items[_id]));
+    props.items = Object.keys(items).filter(_id => {
+      return selection[_id];
+    }).map(_id => Object.assign({
+      isSelected: true,
+    }, items[_id]));
 
-  return props;
-}
+    return props;
+  },
+});
 
-export const CurrentSelectionCard = connect(mapStateToProps)(Card);
+const AllItemsCard = makeCard({
+  headerContent: [(
+    <ButtonGroup key="1">
+      <SelectSwitchButton/>
+      <ScheduleSwitchButton/>
+      <EditSwitchButton/>
+      <DeleteSwitchButton/>
+    </ButtonGroup>
+  ), (<AddItemInputGroup key="2"/>)],
+  itemUi: {
+    buttons: [EditItemButton, DeleteItemButton],
+    checkboxes: [SelectItemCheckbox, ScheduleItemCkeckbox],
+  },
+  mapStateToProps: state => {
+    const props = Object.assign({}, state.items);
+    const items = props.items;
 
-import {default as AllItemsCard} from './all-items-card';
-import {default as AllSelectionsCard} from './all-selections-card';
+    const {_id, isBeingEdited} = state.currentItem;
+    const selection = state.currentSelection.items;
 
-export {AllItemsCard, AllSelectionsCard};
+    props.items = Object.keys(items).map(key => {
+      const item = items[key];
+      return Object.assign({
+        isBeingEdited: isBeingEdited && item._id === _id,
+        isSelected: selection[item._id] ? true : false,
+      }, item);
+    });
+
+    return props;
+  },
+});
+
+const AllSelectionsCard = makeCard({
+  mapStateToProps: state => {
+    const props = Object.assign({}, state.items);
+    const items = props.items;
+    const selections = state.selections.items;
+
+    props.items = Object.keys(selections).map(_id => {
+      return items[selections[_id].itemId];
+    });
+
+    return props;
+  },
+});
+
+setFuncName(CurrentSelectionCard, 'CurrentSelectionCard');
+setFuncName(AllItemsCard, 'AllItemsCard');
+setFuncName(AllSelectionsCard, 'AllSelectionsCard');
+
+export {CurrentSelectionCard, AllItemsCard, AllSelectionsCard};
