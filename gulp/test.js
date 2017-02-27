46c4eb36b65a849585609c0944cc7987893b5100
Jason Lenoble <jason.lenoble@gmail.com>
2017-02-27 16:25:20 +0100

    Setup for remote selenium server

--- gulp/test.js
+++ gulp/test.js
@@ -5,7 +5,6 @@ import cucumber from 'gulp-cucumber';
 import './copy';
 import './bundle';
 import './sass';
-import './selenium';
 
 import {featuresBuildGlob, stepsBuildGlob, stepSupportBuildGlob,
   stepHooksBuildGlob} from './globs';
@@ -27,5 +26,5 @@ export const testFeatures = () => {
     }));
 };
 
-gulp.task('test', gulp.series(gulp.parallel('copy', 'sass', 'bundle',
-  'selenium'), gulp.parallel(test, testFeatures)));
+gulp.task('test', gulp.series(gulp.parallel('copy', 'sass', 'bundle'),
+  gulp.parallel(test, testFeatures)));
--- test/client.js
+++ test/client.js
@@ -4,6 +4,7 @@ const options = {
   desiredCapabilities: {
     browserName: 'firefox',
   },
+  host: 'debian',
 };
 
 export default remote(options);
