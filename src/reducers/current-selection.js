eda62e863fcc9eb81e33b61b0f076666c475862c
Jason Lenoble <jason.lenoble@gmail.com>
2017-03-03 00:15:11 +0100

    3 selection name cases added

--- src/reducers/current-selection.js
+++ src/reducers/current-selection.js
@@ -1,8 +1,10 @@
 import {
-  SELECT_ITEM, UNSELECT_ITEM,
+  SELECT_ITEM, UNSELECT_ITEM, START_NAMING_SELECTION, STOP_NAMING_SELECTION,
+  DISPLAY_SELECTION_NAME,
 } from '../actions';
 
 export function currentSelection (state = {
+  isBeingNamed: false,
   items: {},
 }, action) {
   const _id = action.item ? action.item._id : '';
@@ -25,6 +27,22 @@ export function currentSelection (state = {
     Object.assign(items, state.items);
     delete items[_id];
     return {items};
+
+  case START_NAMING_SELECTION:
+    return Object.assign({}, state, {
+      isBeingNamed: true,
+    });
+
+  case STOP_NAMING_SELECTION:
+    return Object.assign({}, state, {
+      isBeingNamed: false,
+    });
+
+  case DISPLAY_SELECTION_NAME:
+    return Object.assign({}, state, {
+      isBeingNamed: false,
+      item: Object.assign({}, action.item),
+    });
 
   default:
     return state;
