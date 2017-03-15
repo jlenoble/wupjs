ee9a6b4344f91a1a0f23e2982c0fdc414536660d
Jason Lenoble <jason.lenoble@gmail.com>
2017-03-15 14:53:48 +0100

    Removed files after refactoring

--- src/helpers/index.js
+++
@@ -1,3 +0,0 @@
-export * from './make-handle-change';
-export * from './make-is-prop';
-export * from './set-func-name';
--- src/helpers/set-func-name.js
+++
@@ -1,7 +0,0 @@
-export const setFuncName = (Func, name) => {
-  try {
-    Object.defineProperty(Func, 'name', {value: name});
-  } catch (e) {
-    // Do nothing: recover if readonly property error raised
-  }
-};
