9a856db3d9fc779b33158c878a2aec16f07b99c1
Jason Lenoble <jason.lenoble@gmail.com>
2017-03-15 14:50:58 +0100

    Now using only top booleans to switch groups

--- src/components/cards/card-header.jsx
+++ src/components/cards/card-header.jsx
@@ -2,11 +2,15 @@ import React, {PropTypes} from 'react';
 import Item from '../list/item';
 import {itemPropType} from '../list/proptypes';
 import AddGroup from '../groups/add-group';
+import CreateGroup from '../groups/create-group';
+import NameGroup from '../groups/name-group';
 
 const CardHeader = ({item, ui}) => {
   return (
     <div className="card-header">
-      {item && <Item
+      {ui.create && <CreateGroup/>}
+      {ui.name && <NameGroup/>}
+      {ui.title && <Item
         item={item}
         ui={ui.itemUi}
       />}
