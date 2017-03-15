3de2c3f0d408d32f7bd034eeb649ac3da4082af3
Jason Lenoble <jason.lenoble@gmail.com>
2017-03-15 14:43:25 +0100

    Refactored NameGroup
    Was in item-input-groups.jsx, was called NameSelectionInputGroup
    Now using package wupjs-glyph-input-text

---
+++ src/components/groups/name-group.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {newItem, unfocusCurrentItem, displaySelectionName,
+  stopNamingSelection, newSelection} from '../../actions';
+import {GlyphInputText} from 'wupjs-glyph-input-text';
+import {dispatch} from '../../server/store';
+
+const NameGroup = () => (
+  <GlyphInputText
+    placeholder="Name this selection"
+    glyph="save"
+    autoFocus
+    autoClear
+    onFocus={() => dispatch(unfocusCurrentItem())}
+    onSubmit={e => {
+      const value = e.target.value.trim();
+      if (!value) {
+        return;
+      }
+      dispatch(newItem(value))
+      .then(
+        item => dispatch(displaySelectionName(item)),
+        () => dispatch(stopNamingSelection())
+      )
+      .then(
+        ({item}) => dispatch(newSelection(item))
+      );
+    }}
+  />
+);
+
+export default NameGroup;
