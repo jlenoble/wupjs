ebfb1391933e0dcdf138d0c7b1c8731771d5777e
Jason Lenoble <jason.lenoble@gmail.com>
2018-03-10 02:00:24 +0100

    Replaced AllItemsCard with SelectItemsCard

--- src/components/pages/test-page.jsx
+++ src/components/pages/test-page.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Content from '../layouts/content';
-import AllItemsCard from '../cards/all-items-card';
+import SelectItemsCard from '../cards/select-items-card';
 import AllSelectionsCard from '../cards/all-selections-card';
 import CurrentSelectionCard from '../cards/current-selection-card';
 
 export default () => (
   <Content>
-    <AllItemsCard/>
+    <SelectItemsCard/>
     <CurrentSelectionCard/>
     <AllSelectionsCard/>
   </Content>
