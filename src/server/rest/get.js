e5f6de3d71e128a916012662cac2f488c31bb620
Jason Lenoble <jason.lenoble@gmail.com>
2018-03-12 11:51:13 +0100

    Can fetch single item RESTfully

--- src/server/rest/get.js
+++ src/server/rest/get.js
@@ -16,4 +16,19 @@ Object.keys(collections).forEach(key => {
       res.json(items);
     });
   });
+
+  app.get(`/api/${key}/:id`, (req, res) => {
+    const _id = req.params.id;
+
+    Collection.find({_id}, (err, items) => {
+      if (err) {
+        return res.json({
+          status: err.status || 500,
+          message: err.message || 'Internal server error',
+        });
+      }
+
+      res.json(items);
+    });
+  });
 });
