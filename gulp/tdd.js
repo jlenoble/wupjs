6ebfdffeb74390c8f53acb1894c4822ad68ad58d
Jason Lenoble <jason.lenoble@gmail.com>
2018-03-14 01:28:29 +0100

    Add bundling to TDD
    As there are no tests run, bundling task was never defined

--- gulp/tdd.js
+++ gulp/tdd.js
@@ -2,9 +2,10 @@ import gulp from 'gulp';
 
 import './build';
 import './sass';
+import './bundle';
 // import './test';
 import './serve';
 import './watch';
 
-gulp.task('tdd', gulp.series('build', 'sass', 'serve', 'watch'));
+gulp.task('tdd', gulp.series('build', 'sass', 'bundle', 'serve', 'watch'));
 // gulp.task('tdd', gulp.series('test', 'serve', 'watch'));
