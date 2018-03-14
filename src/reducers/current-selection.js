381e5911bcdc877aaa97ae7dd97c11d6ca4aca19
Jason Lenoble <jason.lenoble@gmail.com>
2018-03-14 01:01:56 +0100

    itemsChanged flag
    Handy when updating a selection to show or not save button

--- src/reducers/current-selection.js
+++ src/reducers/current-selection.js
@@ -4,7 +4,9 @@ const {SELECT_ITEM, UNSELECT_ITEM, EDIT_SELECTION, START_NAMING_SELECTION,
   STOP_NAMING_SELECTION, DISPLAY_SELECTION_NAME} = uiActions;
 
 export function currentSelection (state = {
+  selectionId: '',
   isBeingNamed: false,
+  itemsChanged: false,
   items: {},
 }, action) {
   const _id = action.item ? action.item._id : '';
@@ -17,7 +19,7 @@ export function currentSelection (state = {
       return state;
     }
     items[_id] = action.item;
-    return {...state, items: {...state.items, ...items}};
+    return {...state, items: {...state.items, ...items}, itemsChanged: true};
 
   case UNSELECT_ITEM:
     if (!state.items[_id]) {
@@ -25,10 +27,10 @@ export function currentSelection (state = {
     }
     Object.assign(items, state.items);
     delete items[_id];
-    return {...state, items};
+    return {...state, items, itemsChanged: true};
 
   case EDIT_SELECTION:
-    return {...action.item};
+    return {...state, ...action, isBeingUpdated: true};
 
   case START_NAMING_SELECTION:
     return {...state, isBeingNamed: true};
