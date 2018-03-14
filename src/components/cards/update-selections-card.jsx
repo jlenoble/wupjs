bd9b4473ca0b3a781c93696d1a1a1474aab6447b
Jason Lenoble <jason.lenoble@gmail.com>
2018-03-14 14:35:10 +0100

    Don't show selection card if empty

--- src/components/cards/update-selections-card.jsx
+++ src/components/cards/update-selections-card.jsx
@@ -1,10 +1,28 @@
+import React, {Component} from 'react';
 import CardFactory from './card-factory';
+import {connect} from 'react-redux';
 import {getMapOfAllSelections, getItemsFromSelectionMap} from './helpers';
 
-export default CardFactory(
+const SelectionsCard = CardFactory(
   {title: true, itemUi: {inlineRight: ['addSelection']}},
   {inlineRight: ['editSelection', 'deleteItem']},
-  {_id: 'update-selections', title: 'Update selections', filter: state => {
+  {_id: 'update-selections', title: 'Selections', filter: state => {
     return getItemsFromSelectionMap(state, getMapOfAllSelections(state));
   }}
 );
+
+class UpdateSelectionsCard extends Component {
+  render () {
+    const {Card} = this.props;
+    return Card ? <Card/> : <span/>;
+  }
+}
+
+const mapStateToProps = (state, props) => {
+  const hasElements = Object.keys(state.selections.items).length > 0;
+  const Card = hasElements ? SelectionsCard : undefined;
+
+  return {Card};
+};
+
+export default connect(mapStateToProps)(UpdateSelectionsCard);
